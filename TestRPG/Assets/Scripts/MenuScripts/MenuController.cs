cd115b3b61b7401f8cb0fe7ae269b46b448c7552
Sergey <Seregsa2014@gmail.com>
2022-06-21 20:33:49 +0300

    Update
    added game menu and settings

---
+++ TestRPG/Assets/Scripts/MenuScripts/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void ExitPressed()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }
+
+    public void SettingsPressed()
+    {
+        Debug.Log("Settings");
+    }
+
+}
---
+++ TestRPG/Assets/Scripts/MenuScripts/SettingsController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    Resolution[] rsl;
+    List<string> resolutions;
+    public TMPro.TMP_Dropdown dropdown;
+    private bool isFullScreen = true;
+
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width + "x" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutions);
+    }
+
+    public void FullScreenToggle()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+    }
+
+
+}
