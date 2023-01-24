17b2cdaa6b5792782d8bfb7614330722cad2daa0
Sergey <Seregsa2014@gmail.com>
2023-01-25 01:53:27 +0300

    Update
    camera shake added

---
+++ TestRPG/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance { get; private set; }
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeTimer;
+    [Header("Shake params to test")]
+    [SerializeField] private float myShakeTime = 1f;
+    [SerializeField] private float myIntensivity = 1f;
+
+    private void Awake()
+    {
+        Instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensivity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensivity;
+        shakeTimer = time;
+    }
+
+    public void ShakeCamera() // this func need to test params in script
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = myIntensivity;
+        shakeTimer = myShakeTime;
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                //TIME IS OVER
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                    cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+
+}
